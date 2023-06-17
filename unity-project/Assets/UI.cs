a5735ffa9387e5239e3eb74a2fcca885735b191a
Andreas Kobal <kobalandrey@gmail.com>
2023-06-17 01:35:33 +0100

    finalize

--- unity-project/Assets/UI.cs
+++ unity-project/Assets/UI.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using System.Numerics;
+
+using UnityEngine;
+using Web3Unity.Scripts.Library.Ethers.Providers;
+
 
 public class UI : MonoBehaviour
 {
@@ -19,12 +24,44 @@ public class UI : MonoBehaviour
     private Button buttonSelect2;
     private Button buttonSelect3;
 
-    private void Start()
+    private Label wallet;
+    private Label balance; 
+
+    private async void Start()
     {
+        string account = PlayerPrefs.GetString("Account");
+        var provider = RPC.GetInstance.Provider();
+        var getBalance = await provider.GetBalance(account);
+
+        BigInteger weiValue = BigInteger.Parse(getBalance.ToString()); // Example Wei value
+        BigInteger divisor = BigInteger.Pow(10, 18);
+        decimal etherValue = decimal.Divide((decimal)weiValue, (decimal)divisor);
+        
+        Debug.LogError("Account Balance in Wei: " + getBalance);
+        Debug.Log("$MNT: " + etherValue.ToString("0.0"));
+
+
+        
+        
+        
+
+        string formattedString = account.Substring(0, 6) + "..." + account.Substring(account.Length - 4);
+
+        Debug.Log(formattedString);
+        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        wallet = root.Q<Label>("Wallet");
+        balance = root.Q<Label>("Balance");
+
+        wallet.text = formattedString;
+        balance.text = etherValue.ToString("0.0");
+        
+
         // Begin checking NFT balances on start
         checkNFTOwnership.CheckNFTBalance("0x6d0ddf5542cD11B9ee31c8974dD8106a25c3A192", 0);
         checkNFTOwnership.CheckNFTBalance("0x9B8B3f0191a6f607f7b6df35Eb11Bd972Fb238A4", 1);
         checkNFTOwnership.CheckNFTBalance("0x3da421b005d15B6dCDDd7d2bd1c9CC1876638cf7", 2);
+        
     }
 
     private void OnEnable()
@@ -34,6 +71,7 @@ public class UI : MonoBehaviour
         buttonSelect1 = root.Q<Button>("ButtonSelect1");
         buttonSelect2 = root.Q<Button>("ButtonSelect2");
         buttonSelect3 = root.Q<Button>("ButtonSelect3");
+        
 
         buttonSelect1.clicked += () => HandleButtonClick(0);
         buttonSelect2.clicked += () => HandleButtonClick(1);
